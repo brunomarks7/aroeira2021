c130d70a34254a1ae85d1fbab4f3c5d179fd95fb
Bruno Marques <brunomarks@gmail.com>
2020-10-31 01:41:01 -0300

    first

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc7472b0c410b2ac3212e9269346cb27'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fcc792dd-e7d0-41b5-9820-0aa613274249'
+};
